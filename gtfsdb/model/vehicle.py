7a6343323181571a664b9d7bfe951a92c6e7a4fb
Frank Purcell <fxpurcell@gmail.com>
2019-08-29 17:19:54 -0700

    init change

--- gtfsdb/model/vehicle.py
+++ gtfsdb/model/vehicle.py
@@ -6,17 +6,8 @@ from gtfsdb.model.base import Base
 
 
 class Agency(Base):
-    datasource = config.DATASOURCE_GTFS
-    filename = 'agency.txt'
-
-    __tablename__ = 'agency'
+    datasource = config.DATASOURCE_DERIVED
+    __tablename__ = 'vehicles'
 
     id = Column(Integer, Sequence(None, optional=True), primary_key=True, nullable=True)
     agency_id = Column(String(255), index=True, unique=True)
-    agency_name = Column(String(255), nullable=False)
-    agency_url = Column(String(255), nullable=False)
-    agency_timezone = Column(String(50), nullable=False)
-    agency_lang = Column(String(10))
-    agency_phone = Column(String(50))
-    agency_fare_url = Column(String(255))
-    agency_email = Column(String(255))
